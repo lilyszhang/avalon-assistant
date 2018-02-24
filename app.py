69651e7945ec28dc9a773f8dabe0b094d5c536b4
Lily Zhang <lilyszhang96@gmail.com>
2018-02-23 20:26:50 -0600

    retrieve name

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request
+import click
 import json
 app = Flask(__name__)
 
 @app.route('/join', methods=['GET','POST'])
 def join():
-    print 'hi'
+    json = request.get_json()
+    click.echo(json['name'])
+    return 'placeholder'
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True, host='0.0.0.0')
